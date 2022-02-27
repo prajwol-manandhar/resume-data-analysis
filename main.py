08b7c29c05f8a1d0108a04da0da60c01d19076c2
prajwol-manandhar <manandharprajwal5@gmail.com>
2022-02-27 15:51:40 +0545

    Analysis via user input of userid

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import firebase_admin
 from firebase_admin import credentials
 
-#from insert_document import insert_user
-from get_skills import user_skills
-#from keywords import tech_keywords
+from analysis import get_user_skills, get_tech_result, get_management_result, get_softskill_result, feedback
 
 cred = credentials.Certificate("service_key.json")
 firebase_admin.initialize_app(cred)
 
-user_skills()
+userid = input('Please enter the userID: ')
+
+user_skill_string = get_user_skills(userid)
+tech_result = get_tech_result(user_skill_string)
+management_result = get_management_result(user_skill_string)
+softskill_result = get_softskill_result(user_skill_string)
+
+feedback(tech_result, management_result, softskill_result)
