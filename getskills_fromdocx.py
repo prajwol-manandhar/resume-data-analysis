8b761ce3c5f0523d5aa9bdbc3ee1a00543e365b0
Prajwol Manandhar <prajwolmanandhar@outlook.com>
2022-02-28 15:49:19 +0545

    Create getskills_fromdocx

---
+++ getskills_fromdocx.py
@@ -0,0 +1,66 @@
+import firebase_admin
+from firebase_admin import credentials, firestore
+import docx2txt
+import nltk
+from analysis import get_tech_result, get_management_result, get_softskill_result, feedback
+
+nltk.download('stopwords')
+nltk.download('punkt')
+
+cred = credentials.Certificate("service_key.json")
+firebase_admin.initialize_app(cred)
+
+tech_keyword = firestore.client().collection('keyword').document('technology').get().to_dict()['key']
+management_keyword = firestore.client().collection('keyword').document('management').get().to_dict()['key']
+softskill_keyword = firestore.client().collection('keyword').document('softskill').get().to_dict()['key']
+
+
+def extract_text_from_docx(docx_path):
+    txt = docx2txt.process(docx_path)
+    if txt:
+        return txt.replace('\t', ' ')
+    return None
+
+
+def extract_skills(input_text):
+    stop_words = set(nltk.corpus.stopwords.words('english'))
+    word_tokens = nltk.tokenize.word_tokenize(input_text)
+
+    # remove the stop words
+    filtered_tokens = [w for w in word_tokens if w not in stop_words]
+
+    # remove the punctuation
+    filtered_tokens = [w for w in word_tokens if w.isalpha()]
+
+    # generate bigrams and trigrams (such as artificial intelligence)
+    bigrams_trigrams = list(map(' '.join, nltk.everygrams(filtered_tokens, 2, 3)))
+
+    # create a set to keep the results in.
+    found_skills = set()
+
+    # search for each token in our skills database
+    for token in filtered_tokens:
+        if token.lower() in tech_keyword:
+            found_skills.add(token)
+
+    # search for each bigram and trigram in our skills database
+    for ngram in bigrams_trigrams:
+        if ngram.lower() in tech_keyword:
+            found_skills.add(ngram)
+    
+    print('Tech Keywords: ', tech_keyword)
+    return found_skills
+
+
+if __name__ == '__main__':
+    text = extract_text_from_docx('CV.docx')
+    skills = extract_skills(text)
+    print('Result: ', skills) 
+
+user_skill_string = str(skills)
+
+tech_result = get_tech_result(user_skill_string)
+management_result = get_management_result(user_skill_string)
+softskill_result = get_softskill_result(user_skill_string)
+
+feedback(tech_result, management_result, softskill_result)
